63de22235ccfcc090a60779749fb0df980dcf48e
Matthew Cottrell <mattcottrell93@hotmail.co.uk>
2017-05-01 10:02:22 +0000

    added before_action into articles controller, and prepared for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     
     def index
         @articles = Article.all
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+
     end
     
     def create
@@ -24,13 +25,10 @@ class ArticlesController < ApplicationController
     end
     
     def show
-       @article = Article.find(params[:id])
-       
         
     end
     
     def update 
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -41,7 +39,6 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-       @article = Article.find(params[:id])
        @article.destroy
        flash[:danger] = "Article was successfully deleted"
        redirect_to articles_path
@@ -50,6 +47,9 @@ class ArticlesController < ApplicationController
     
     
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
     def article_params
         params.require(:article).permit(:title, :description)
     end
